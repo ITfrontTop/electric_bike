c55d229ec12d74a2f3e9edd82d2d8234c2584536
ITfrontTop <Fine1337@ukr.net>
2023-11-08 12:19:33 +0200

    added price, category, brand, color

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // Swiper sliders:
-const swiper = new Swiper ('.header-slider__swiper', {
+const swiper = new Swiper('.header-slider__swiper', {
     speed: 1000,
     loop: true,
     // autoplay: {
@@ -16,7 +16,7 @@ const swiper = new Swiper ('.header-slider__swiper', {
     },
 })
 
-const novelties = new Swiper ('.novelties-slider', {
+const novelties = new Swiper('.novelties-slider', {
     speed: 1000,
     loop: true,
     slidesPerView: 1,
@@ -27,21 +27,21 @@ const novelties = new Swiper ('.novelties-slider', {
     },
     breakpoints: {
         720: {
-          slidesPerView: 2,
-          spaceBetween: 40,
+            slidesPerView: 2,
+            spaceBetween: 40,
         },
         1240: {
             slidesPerView: 3,
             spaceBetween: 40,
-          },
+        },
     },
 })
 
 
-const best_winter = new Swiper ('.best-winter__slider', {
+const best_winter = new Swiper('.best-winter__slider', {
     speed: 1000,
     loop: true,
-    slidesPerView: 1, 
+    slidesPerView: 1,
     spaceBetween: 40,
     navigation: {
         nextEl: '.swiper-button-next',
@@ -49,8 +49,8 @@ const best_winter = new Swiper ('.best-winter__slider', {
     },
     breakpoints: {
         720: {
-          slidesPerView: 2,
-          spaceBetween: 40,
+            slidesPerView: 2,
+            spaceBetween: 40,
         },
         1340: {
             slidesPerView: 3,
@@ -64,7 +64,7 @@ const best_winter = new Swiper ('.best-winter__slider', {
 })
 
 
-const equipment = new Swiper ('.equipment__slider', {
+const equipment = new Swiper('.equipment__slider', {
     speed: 1000,
     loop: true,
     slidesPerView: 1,
@@ -75,8 +75,8 @@ const equipment = new Swiper ('.equipment__slider', {
     },
     breakpoints: {
         720: {
-          slidesPerView: 2,
-          spaceBetween: 40,
+            slidesPerView: 2,
+            spaceBetween: 40,
         },
         1240: {
             slidesPerView: 3,
@@ -87,10 +87,10 @@ const equipment = new Swiper ('.equipment__slider', {
 
 
 // header event listener
-document.querySelector('.menu-list').addEventListener('click', function(e) {
+document.querySelector('.menu-list').addEventListener('click', function (e) {
     e.preventDefault()
     console.log(e.target)
-    if(e.target.classList.contains('menu-link')) {
+    if (e.target.classList.contains('menu-link')) {
         const href = e.target.getAttribute('href')
         document.querySelector(href).scrollIntoView({
             behavior: 'smooth',
@@ -100,9 +100,9 @@ document.querySelector('.menu-list').addEventListener('click', function(e) {
 
 
 // footer event listener
-document.querySelector('.footer__content-menu-items').addEventListener('click', function(e) {
+document.querySelector('.footer__content-menu-items').addEventListener('click', function (e) {
     e.preventDefault()
-    if(e.target.classList.contains('footer__content-link')) {
+    if (e.target.classList.contains('footer__content-link')) {
         const href = e.target.getAttribute('href')
         document.querySelector(href).scrollIntoView({
             behavior: 'smooth',
@@ -118,19 +118,19 @@ const closeBtn = document.querySelector('.menu-more__btn-link')
 const menuMore = document.querySelector('.menu-more')
 const overlay = document.querySelector('.menu-more__overlay')
 
-const openMenuMore = function(e) {
+const openMenuMore = function (e) {
     e.preventDefault()
     // menuMore.classList.remove('hidden')
     overlay.classList.remove('hidden')
     menuMore.classList.add('show')
-    
+
 }
 
-const closeMenuMore = function() {
+const closeMenuMore = function () {
     // menuMore.classList.add('hidden')
     overlay.classList.add('hidden')
     menuMore.classList.remove('show')
-    
+
 }
 
 menuMoreBtn.forEach(button => button.addEventListener('click', openMenuMore))
@@ -159,7 +159,7 @@ function boxHandler(e) {
     let currentContent = e.target.nextElementSibling
     currentBox.classList.toggle('active')
 
-    if(currentBox.classList.contains('active')) {
+    if (currentBox.classList.contains('active')) {
         // Чтобы box раскрывался плавно
         currentContent.style.maxHeight = currentContent.scrollHeight + 'px'
     } else {
@@ -186,18 +186,18 @@ const inputForm = document.querySelectorAll('.footer__subscription-input')
 // Чтобы блокировать скролл нужно body
 // const popup = document.querySelector('.popup')
 
-const openPopup = function() {
+const openPopup = function () {
     popup.classList.add('open')
 }
 
-const closePopup = function() {
+const closePopup = function () {
     popup.classList.remove('open')
 }
 
 // Проверка на валидность input
-const validateForm = function() {
-    for(const input of inputForm) {
-        if(input.validity.valid) {
+const validateForm = function () {
+    for (const input of inputForm) {
+        if (input.validity.valid) {
             return true
         } else {
             return false
@@ -205,8 +205,8 @@ const validateForm = function() {
     }
 }
 
-popupLink.forEach(button => button.addEventListener('click', function() {
-    if(validateForm()) {
+popupLink.forEach(button => button.addEventListener('click', function () {
+    if (validateForm()) {
         openPopup()
     } else {
         return
@@ -219,4 +219,108 @@ popupBody.addEventListener('click', closePopup)
 
 
 
-// показать ещё
+// кнопка - показать ещё
+// находим кнопку
+const showMoreCategory = document.querySelector('.show-more-category')
+
+
+// находим все пункты
+const productsLengthCategory = document.querySelectorAll('.category__item-label').length
+
+// количество начальное количество пунктов
+let itemsCategory = 5
+
+
+showMoreCategory.addEventListener('click', () => {
+    // добавляем количество пунктов
+    itemsCategory += 5
+
+    // найти все элементы
+    const arrayCategory = Array.from(document.querySelector('.category__item').children)
+
+    // элементы которые нужно видеть
+    const visItemsCategory = arrayCategory.slice(0, itemsCategory)
+
+    // делаем чтобы видеть елементы
+    visItemsCategory.forEach(el => el.classList.add('is-visible'))
+
+    // удаляем кнопку когда елеметы заканчиваються
+    if (visItemsCategory.length === productsLengthCategory) {
+        showMoreCategory.style.display = 'none'
+    }
+
+})
+
+
+const showMoreBrand = document.querySelector('.show-more-brand')
+const productsLengthBrand = document.querySelectorAll('.brand-aside__item-label').length
+let itemsBrand = 5
+
+
+showMoreBrand.addEventListener('click', () => {
+    itemsBrand += 5
+
+    const arrayBrand = Array.from(document.querySelector('.brand-aside__item').children)
+
+    const visItemsBrand = arrayBrand.slice(0, itemsBrand)
+
+    visItemsBrand.forEach(el => el.classList.add('is-visible'))
+
+    if (visItemsBrand.length === productsLengthBrand) {
+        showMoreBrand.style.display = 'none'
+    }
+})
+
+
+// цены которые меняються ползунками
+const rangeInput = document.querySelectorAll('.range-input input'),
+priceInput = document.querySelectorAll('.field input'),
+progress = document.querySelector('.slider .progress')
+
+let priceGap = 1000;
+
+priceInput.forEach(input => {
+    input.addEventListener('input', e => {
+        // получаем значения от двох ползунков и преобразуем в цифры
+        let minVal = parseInt(priceInput[0].value),
+            maxVal = parseInt(priceInput[1].value)
+
+        if ((maxVal - minVal >= priceGap) && maxVal <= 1500000) {
+            if(e.target.className === 'input-min') {
+                rangeInput[0].value = minVal
+                progress.style.left = (minVal / rangeInput[0].max) * 100 + '%'
+            } else {
+                rangeInput[1].value = maxVal
+                progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + '%'
+            }
+        }
+
+
+
+    })
+})
+
+rangeInput.forEach(input => {
+    input.addEventListener('input', e => {
+        // получаем значения от двох ползунков и преобразуем в цифры
+        let minVal = parseInt(rangeInput[0].value),
+            maxVal = parseInt(rangeInput[1].value)
+
+        if (maxVal - minVal < priceGap) {
+            if(e.target.className === 'range-min') {
+                rangeInput[0].value = maxVal - priceGap
+            } else {
+                rangeInput[1].value = minVal + priceGap 
+            }
+        } else {
+            // чтобы линия двигалась за ползунками
+            priceInput[0].value = minVal
+            priceInput[1].value = maxVal
+            progress.style.left = (minVal / rangeInput[0].max) * 100 + '%'
+            progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + '%'
+        }
+
+
+
+    })
+})
